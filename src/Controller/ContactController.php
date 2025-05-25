8a2939a8170cc9800dacfba504d75109da4504d3
slim <slimabida21@gmail.com>
2025-05-25 13:27:48 +0200

    Mise en place du formulaire de contact et modification dans le Home page

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,17 +2,60 @@
 
 namespace App\Controller;
 
+use App\Form\ContactTypeForm;
+use App\Service\EmailService; // Ensure this class exists in the specified namespace
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+
 
 final class ContactController extends AbstractController
 {
-    #[Route('/contact', name: 'app_contact')]
-    public function index(): Response
+    private $emailService;
+
+    public function __construct(EmailService $emailService)
     {
+        $this->emailService = $emailService;
+    }
+    #[Route('/contact', name: 'app_contact')]
+    public function index(Request $request): Response
+    {
+        // Création du formulaire de contact à partir de la classe ContactType
+        $form = $this->createForm(ContactTypeForm::class);
+        // Traitement des données POST
+        $form->handleRequest($request);
+        // Vérification de la soumission du formulaire et de sa validité
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Récupération des données du formulaire
+            $data = $form->getData();
+            // Envoi du mail
+           
+            $this->emailService->sendTemplatedEmail(
+                
+                'contact.ecoride9@gmail.com',
+                'Message via le formulaire de contact',
+                'emails/contact.html.twig',
+                [
+                    'firstname' => $data['firstname'],
+                    'lastname' => $data['lastname'],
+                    'senderEmail' => $data['email'],
+                    'subject' => $data['subject'],
+                    'message' => $data['message'],
+                ]
+                
+                
+              
+            );
+
+            // Ajout d'un message flash
+            $this->addFlash('success', 'Votre message a bien été envoyé.');
+            // Redirection vers la page de contact
+            return $this->redirectToRoute('app_contact');
+        }
+        // Affichage du formulaire de contact   
         return $this->render('contact/contact.html.twig', [
-            'controller_name' => 'ContactController',
+            'formContact' => $form->createView(),
         ]);
     }
 }
---
+++ src/Form/ContactTypeForm.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class ContactTypeForm extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('firstname', TextType::class, [
+                'label' => 'Prénom',
+                'label_attr' => [
+                    'class' => 'text-success',
+                ],
+                'attr' => [
+                    'placeholder' => 'Votre prénom',
+                ],  
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => 'Nom',
+                'label_attr' => [
+                    'class' => 'text-success',
+                ],
+                'attr' => [
+                    'placeholder' => 'Votre nom',
+                ],
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+                'label_attr' => [
+                    'class' => 'text-success',
+                ],
+                'attr' => [
+                    'placeholder' => 'Votre email',
+                ],
+            ])
+            ->add('subject', TextType::class, [
+                'label' => 'Sujet',
+                'label_attr' => [
+                    'class' => 'text-success',
+                ],
+                'attr' => [
+                    'placeholder' => 'Sujet de votre message',
+                ],
+            ])
+            ->add('message', TextareaType::class, [
+                'label' => 'Message',
+                'label_attr' => [
+                    'class' => 'text-success',
+                ],
+            ])
+            ;
+    }
+
+    
+    
+    
+    
+    
+    
+    
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
