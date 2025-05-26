306e3ef34207411fc4263832279ac1e4212b9b58
slim <slimabida21@gmail.com>
2025-05-26 12:33:39 +0200

    MAJ du security.yaml et du login authenticator en créant une rediraction vers app_login aorès déconnexion de l'utilisateur

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -9,7 +9,7 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class LoginController extends AbstractController
 {
-    #[Route(path: '/login', name: 'app_login')]
+    #[Route(path: '/connexion', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
         // if ($this->getUser()) {
@@ -24,7 +24,7 @@ class LoginController extends AbstractController
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
 
-    #[Route(path: '/logout', name: 'app_logout')]
+    #[Route(path: '/deconnexion', name: 'app_logout')]
     public function logout(): void
     {
         
