b94f05398baf31080351fa6d71fae7dbf31d4956
slim <slimabida21@gmail.com>
2025-05-26 17:22:33 +0200

    Correction dans le login.html.twig test de connexion après inscription OK redirection vers le homePage OK

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -12,9 +12,9 @@ class LoginController extends AbstractController
     #[Route(path: '/connexion', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
+         if ($this->getUser()) {
+           return $this->redirectToRoute('app_home');
+         }
 
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
@@ -22,6 +22,7 @@ class LoginController extends AbstractController
         $lastUsername = $authenticationUtils->getLastUsername();
 
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+       
     }
 
     #[Route(path: '/deconnexion', name: 'app_logout')]
