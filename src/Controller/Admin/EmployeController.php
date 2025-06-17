b5d7bef98161622e4af5dc0e496f4b17ecfd5a1b
slim <slimabida21@gmail.com>
2025-06-17 10:29:38 +0200

    création de la classe LoginSuccessHandler pour permttre d'afficher la page avis_lsit et faire du routing selon le ROLE utiliser lors de la connexion dans le projet j'utilise cette classe pour pouvoir redireger un User ayant le role employ a accéder sur l'interface employe au lieu de l'interface User

--- src/Controller/Admin/EmployeController.php
+++ src/Controller/Admin/EmployeController.php
@@ -4,6 +4,9 @@ namespace App\Controller\Admin;
 
 use App\Repository\AvisRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Avis;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
@@ -12,7 +15,7 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 #[Route('/admin/employe')]
 #[IsGranted('ROLE_EMPLOYE')]
  class EmployeController extends AbstractController
-{   
+{
  /**
   * Cette route va nous permettre les avis avec le statut en attente de validation 
   */
@@ -37,38 +40,46 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
      * Cette route et méthode va npous permettre de valider l'avis passager 
      */
 
-    #[Route('/avis/{id}/valider', name: 'app_admin_employe_avis_valider')]
-    public function validerAvis(AvisRepository $avisRepository): Response
+    #[Route('/avis/{id}/valider', name: 'app_admin_employe_avis_valider', methods:['POST'])]
+    public function validerAvis(Request $request, Avis $avis, EntityManagerInterface $em): Response
     {
-        // On récupère la liste des avis en attente de validation  via le TrajetRepository avec le findBy 
 
-        $avisEnAttente = $avisRepository->findBy(['statut' => 'en_attente_validation'], ['date' => 'DESC']);
+       // On vérifie le token CSRF popur la sécurité // 
+       if($this->isCsrfTokenValid('valider'.$avis->getId(), $request->request->get('_token'))){
         
-        
-        return $this->render('admin/employe/avis_list.html.twig', [
-            'avisList' => $avisEnAttente,
-            'page_title' => 'Avis en attente de validation', 
-        ]);
+        $avis->setStatut('approuve');
+        $em->flush();
+
+    
+    }else{
+        $this->addFlash('error', 'Token CSRF invalide.');
+    
     }
+        
+    return $this->redirectToRoute('app_admin_employe_avis_list');
+ }
 
+/**
+ * Cette route et méthode va nous permettre de refuser un avis par l'employe
+ */
+#[Route('/avis/{id}/refuser', name: 'app_admin_employe_avis_refuser', methods: ['POST'])]
+    public function refuserAvis(Request $request, Avis $avis, EntityManagerInterface $em): Response
+    {
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+       // On vérifie le token CSRF popur la sécurité // 
+       if($this->isCsrfTokenValid('valider'.$avis->getId(), $request->request->get('_token'))){
+        
+        $avis->setStatut('rejete');
+        $em->flush();
+        $this->addFlash('success', "L'avis a été refusé. ");
+    
+    }else{
+        $this->addFlash('error', 'Token CSRF invalide.');
+    
+    }
+        
+    return $this->redirectToRoute('app_admin_employe_avis_list');
+ }
 
 
 
